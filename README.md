fe00b40f846a60b11a0f1484c1f10a52d1ff20be
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2022-11-10 11:01:16 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # gridware-web
+
+![Platform: Web](https://img.shields.io/badge/platform-Web-yellowgreen?style=flat-square)
+![Framework: Vue](https://img.shields.io/badge/Framework-Vue-4FC08D?style=flat-square&logo=vue.js) ![Tooling: vite](https://img.shields.io/badge/Build-vite-646CFF?style=flat-square&logo=vite)
+![package: pnpm](https://img.shields.io/badge/Package_manager-pnpm-CB3837?style=flat-square&logo=pnpm)
+
+An online version of GridWare.
+This project is not completed yet; I will be developing this tool during my doctoral course. The overall progress would take about 8 months.
+
+Once finished, I will update the README file and publish a release version.
+
---
+++ vite.config.js
@@ -0,0 +1,89 @@
+import { visualizer } from "rollup-plugin-visualizer";
+import { defineConfig } from "vite";
+import viteCompression from "vite-plugin-compression";
+import { VitePWA } from "vite-plugin-pwa";
+import legacy from "@vitejs/plugin-legacy";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    legacy({
+      targets: [
+        "Android >= 39",
+        "Chrome >= 50",
+        "Safari >= 10.1",
+        "iOS >= 10.3",
+        "> 1%",
+      ],
+    }),
+    VitePWA({
+      injectRegister: "auto",
+      registerType: "autoUpdate",
+      includeManifestIcons: true,
+      includeAssets: ["favicon/*.png"],
+      manifest: {
+        name: "GridWare Web",
+        short_name: "GridWareWeb",
+        description: "GridWare, but online",
+        lang: "zh-CN",
+        theme_color: "#fff",
+        icons: [
+          {
+            src: "/favicon/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+          },
+          {
+            src: "/favicon/maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/favicon/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/favicon/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /(.*?)\.(png|jpe?g|svg|gif|webp|ico)$/i,
+            handler: "StaleWhileRevalidate",
+            options: {
+              cacheName: "image-cache",
+            },
+          },
+          {
+            urlPattern: /(.*?)\.(js|css|json)$/i,
+            handler: "NetworkFirst",
+            options: {
+              cacheName: "asset-cache",
+              networkTimeoutSeconds: 5,
+            },
+          },
+        ],
+      },
+      // uncomment to unregister service worker
+      selfDestroying: true,
+    }),
+    viteCompression(),
+    visualizer(),
+  ],
+  build: {
+    emptyOutDir: true,
+    minify: "terser",
+    terserOptions: {
+      toplevel: true,
+      safari10: true,
+    },
+  },
+});
