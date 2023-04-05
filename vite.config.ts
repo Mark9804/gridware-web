74bf2c60b8ef155c9d4a1565789116336feb7f3b
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2023-04-05 16:57:49 +0900

    chore: update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,20 @@ import viteCompression from 'vite-plugin-compression';
 import { VitePWA } from 'vite-plugin-pwa';
 import legacy from '@vitejs/plugin-legacy';
 import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      resolvers: [NaiveUiResolver()],
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()],
+    }),
     legacy({
       targets: [
         'Android >= 39',
